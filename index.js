e55c09a46831916a6a8dc3b1a6bb4b0455f5067d
ajakcyer <ajakcyer97@gmail.com>
2020-10-27 15:28:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const text = document.getElementById('text')
+
+document.addEventListener("DOMContentLoaded", function() {
+    text.textContent = 'This is really cool!'
+  });
+
+
